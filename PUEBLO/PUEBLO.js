4db8f7b7c1877507633f882f1bb8549c1d62d071
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-26 03:26:51 -0300

    Se agrega ejercicio pueblo

---
+++ PUEBLO/PUEBLO.js
@@ -0,0 +1,58 @@
+var vp = document.getElementById("pueblo");
+var papel = vp.getContext("2d");
+
+var fondo = {
+  url: "FONDO.png",
+  cargaOK: false
+}
+var vaca = {
+  url: "VACA.png",
+  cargaOK: false
+};
+
+var cantidad = aleatorio(1, 10);
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+function cargarFondo()
+{
+  fondo.cargaOK = true;
+  dibujar();
+}
+function cargarVacas()
+{
+  vaca.cargaOK = true;
+  dibujar();
+}
+
+function dibujar()
+{
+  if(fondo.cargaOK)
+  {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  if(vaca.cargaOK)
+  {
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
+    {
+      var x = aleatorio(0, 7);
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
+      papel.drawImage(vaca.imagen, x, y);
+    }
+  }
+}
+function aleatorio(min, maxi)
+{
+  var resultado;
+  resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+  return resultado;
+}
