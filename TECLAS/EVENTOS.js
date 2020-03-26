ff4a8b343d0c4134f31a80c3f6d93cb94348d29c
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-26 03:20:49 -0300

    Se agrega ejercicio teclas

---
+++ TECLAS/EVENTOS.js
@@ -0,0 +1,53 @@
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+
+console.log(teclas);
+
+document.addEventListener("keydown", dibujarTeclado);
+
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+
+dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function dibujarTeclado(evento) {
+
+  var colorcito = "brown";
+  var movimiento = 5;
+
+  switch (evento.keyCode) {
+    case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
+    break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y = y + movimiento;
+    break;
+    case teclas.LEFT:
+      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+      x = x - movimiento;
+    break;
+    case teclas.RIGHT:
+      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+      x = x + movimiento;
+    break;
+  }
+
+}
